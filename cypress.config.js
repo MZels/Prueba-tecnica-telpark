ba08ce67f368f0fd7495cf3aeed1bc9444bb4fb8
MZels <miguelzels.mz@gmail.com>
2024-10-22 19:31:08 +0200

    se añade cross browser testing al  ejercicio 1

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
+  trashAssetsBeforeRuns: false,
+  viewportWidth: 1980,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
-});
+})
--- cypress/e2e/ejercicio_1.cy.js
+++ cypress/e2e/ejercicio_1.cy.js
@@ -13,26 +13,38 @@ Desarrolla un script que:
 
 /// <reference types="cypress" />
 
-const searchText = 'Automatización de Pruebas' 
+const searchText = 'Automatización de Pruebas'
 const baseUrl = 'https://www.google.com/'
 
-const login = () => {
-  cy.session('user', () => {
-    cy.visit(baseUrl)
-    cy.get("#W0wltc").click()
+/*
+  Hasta donde yo se, con el comando de ejecución "open" no se puede cambiar de navegador, ya que primero seleccionas el navegador sobre el que se ejecutarán las pruebas
+  y luego eliges la spec a ejecutar con dicho navegador.
+
+  Es por esto que para la ejecución del mismo test en distintos navegadores y con diferentes resoluciones he definido una serie de scripts en "package.json".
+  A cada comando se le indica mediante opciones, el navegador con el que ejecutar los tests y la configuración de resolución de ventana.
+
+  Por último, para facilitar la ejecución de esta prueba técnica, he definido un script que ejecuta todos los demás, de esa manera podremos ejecutar el test con las
+  diferentes configuraciones mediante el siguiente comando:
+
+          > npm run e2e:all
+*/
+
+describe('Automatización Web Básica', () => {
+
+  // En caso de hacer más tests de búsquedas en google sería útil saltarse el aviso de privacidad de google, por eso guardo la sesión.
+  before(() => {
+    cy.session('anonymous', () => {
+      cy.visit(baseUrl)
+      cy.get("#W0wltc").click()
+    })
   })
-}
 
-// En caso de hacer más tests de búsquedas en google sería útil saltarse el aviso de privacidad de google, por eso guardo la sesión.
-beforeEach(() => {
-  cy.session('user', login)
-})
-
-describe('Ejercicio 1', () => {
-  it('Automatización Web Básica', () => {
+  it(`${Cypress.browser.name} [${Cypress.config("viewportWidth")}x${Cypress.config("viewportHeight")}]`, () => {
     cy.visit(baseUrl) // visitamos google
     cy.get('#APjFqb').type(`${searchText}{enter}`) // escribimos en la barra de búsqueda
     cy.title().should('contains', searchText) // comprobamos que el título de la página contenga el término buscado
-    cy.screenshot('Pagina_de_resultados') // hacemos una captura de pantalla de los resultados de la búsqueda (se guarda en la carpeta por defecto en este caso)
+    cy.window().then((win) => {
+      cy.screenshot(`${Cypress.browser.name}/${win.innerWidth}x${win.innerHeight}`) // hacemos una captura de pantalla de los resultados de la búsqueda
+    })
   })
 })
